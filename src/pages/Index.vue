14b1f50587eaf7b91289303d4c04bc2c53d5c11b
Tim Halley <timothy.halley@gmail.com>
2021-10-07 11:24:51 -0700

    OCT7 - add Gettysburg Address

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,54 @@
 <template>
   <Layout>
-
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
     <h1>Hello, world!</h1>
 
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      Nov. 19th 1863. “Four score and seven years ago our fathers brought forth
+      on this continent a new nation, conceived in liberty, and dedicated to the
+      proposition that all men are created equal. “Now we are engaged in a great
+      civil war, testing whether that nation, or any nation so conceived and so
+      dedicated, can long endure. We are met on a great battlefield of that war.
+      We have come to dedicate a portion of that field as a final resting place
+      for those who here gave their lives that that nation might live. It is
+      altogether fitting and proper that we should do this. “But in a larger
+      sense we cannot dedicate, we cannot consecrate, we cannot hallow this
+      ground. The brave men, living and dead, who struggled here have
+      consecrated it, far above our poor power to add or detract. The world will
+      little note, nor long remember, what we say here, but it can never forget
+      what they did here. It is for us the living, rather, to be dedicated here
+      to the unfinished work which they who fought here have thus far so nobly
+      advanced. It is rather for us to be here dedicated to the great task
+      remaining before us,that from these honored dead we take increased
+      devotion to that cause for which they gave the last full measure of
+      devotion, that we here highly resolve that these dead shall not have died
+      in vain, that this nation, under God, shall have a new birth of freedom,
+      and that government of the people, by the people, for the people, shall
+      not perish from the earth.”
     </p>
 
     <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
+        >Gridsome Docs</a
+      >
+      <a
+        href="https://github.com/gridsome/gridsome"
+        target="_blank"
+        rel="noopener"
+        >GitHub</a
+      >
     </p>
-
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+    title: "Hello, world!",
+  },
+};
 </script>
 
 <style>
